1e2a81154b62110fdc1d06ad6a5eec88bd3867bd
Elia Pasquali <45105755+eliapasquali@users.noreply.github.com>
2023-11-11 09:24:06 +0100

    station list

--- create.sql
+++ create.sql
@@ -1,18 +1,21 @@
 CREATE TABLE "ParkingArrival" (
 	"Timestamp"	REAL NOT NULL,
 	"Plate"	TEXT NOT NULL,
+	"StationID" INTEGER NOT NULL,
 	PRIMARY KEY("Timestamp")
 );
 
 CREATE TABLE "AbusiveParking" (
 	"Timestamp"	REAL NOT NULL,
 	"Plate"	TEXT NOT NULL,
+	"StationID" INTEGER NOT NULL,
 	PRIMARY KEY("Timestamp")
 );
 
 CREATE TABLE "DriverOut" (
 	"Timestamp"	REAL NOT NULL,
 	"Plate"	TEXT NOT NULL,
+	"StationID" INTEGER NOT NULL,
 	PRIMARY KEY("Timestamp")
 );
 
@@ -22,6 +25,20 @@ CREATE TABLE "Profiling" (
 	"Gender"	TEXT NOT NULL,
 	"Emotion"	TEXT NOT NULL,
 	"Plate"	TEXT NOT NULL,
+	"StationID" INTEGER NOT NULL,
 	PRIMARY KEY("Plate")
 );
 
+CREATE TABLE "Cars" (
+	"Plate"	TEXT NOT NULL,
+	"Manifacturer"	TEXT NOT NULL,
+	"Engine"	TEXT NOT NULL,
+	PRIMARY KEY("Plate")
+)
+
+CREATE TABLE "Stations" (
+	"StationID"	INTEGER NOT NULL,
+	"Latitude"	REAL NOT NULL,
+	"Longitude"	REAL NOT NULL,
+	PRIMARY KEY("StationID")
+)
